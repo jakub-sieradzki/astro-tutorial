f9f3a048b1d36eee8f9d3586dc21a6eb35fd2364
Jakub Sieradzki <rootekstudio@outlook.com>
2026-05-21 18:45:37 +0200

    Astro API

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,6 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://voluble-cactus-dc9452.netlify.app"
+});
